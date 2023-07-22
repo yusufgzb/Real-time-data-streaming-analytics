54ae4ec87fb5692aa28b8c7ed74cb521625fc290
Yusuf Gözübüyük <44902732+yusufgzb@users.noreply.github.com>
2023-07-22 06:38:39 +0300

    Update 13-GCS_BigQuery_json.py

--- airflow/13-GCS_BigQuery_json.py
+++ airflow/13-GCS_BigQuery_json.py
@@ -1,13 +1,11 @@
 from airflow import DAG
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
-
-from airflow.providers.google.cloud.operators.bigquery import BigQueryExecuteQueryOperator
-
 from airflow.utils.dates import days_ago
+from airflow.operators.bash_operator import BashOperator
 
 
-PROJE_AD = "eloquent-petal-379005"
-DB_AD = "analiz"
+PROJE_AD = "steadfast-wares-388305"
+DB_AD = "analytics"
 
 with DAG(
     dag_id="bigquery_data_load",
@@ -15,10 +13,11 @@ with DAG(
     catchup=False,
     start_date=days_ago(1)
 ) as dag:
+
     load_data = GCSToBigQueryOperator(
         task_id ="load_data",
-        bucket = "bigquery_json2/{{ ds }}",
-        source_objects=["*"],
+        bucket = "airflow11/{{ ds }}",
+        source_objects=["*.json"],
         source_format="NEWLINE_DELIMITED_JSON",
         destination_project_dataset_table= f"{PROJE_AD}.{DB_AD}.butunveri",
         create_disposition = "CREATE_IF_NEEDED",
@@ -27,17 +26,4 @@ with DAG(
         # WRITE_EMPTY
         gcp_conn_id="gcp_test"
     )
-    sorgu ="select * from eloquent-petal-379005.analiz.butunveri where sicaklik > 10"
-    
-    create_new_table = BigQueryExecuteQueryOperator(
-        task_id = "create_new_table",
-        sql=sorgu ,
-        destination_dataset_table= f"{PROJE_AD}.{DB_AD}.analizli_tablo",
-        write_disposition="WRITE_TRUNCATE",
-        create_disposition = "CREATE_IF_NEEDED",
-        use_legacy_sql=False, 
-        gcp_conn_id="gcp_test"
-
-
-    )
-    load_data >> create_new_table
+    load_data
